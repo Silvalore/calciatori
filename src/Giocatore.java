efc1b30af082efec16f5485d8189d29a468faeba
silva.21140 <silva.lorenzo.studente@itispaleocapa.it>
2024-10-07 12:42:22 +0200

    .

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -4,6 +4,31 @@ public class Giocatore {
     private int gol;
 
 public Giocatore( String nomeGiocatore, boolean isCapitano, int goal){
-
+    nome= nomeGiocatore;
+    capitano=isCapitano;
+    gol=goal;
 }
+public String getNome(){
+    return nome;
+    }
+
+    public int getGol() {
+        return gol;
+    }
+
+    public boolean getCapitano() {
+        return capitano;
+    }
+
+    public void setCapitano(boolean capitano) {
+        this.capitano = capitano;
+    }
+
+    public void setGol(int gol) {
+        this.gol = gol;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
