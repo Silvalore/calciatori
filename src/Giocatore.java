32ff3ed6d3c49e5c8ae7182bc62f72a6471cd2a6
silva.21140 <silva.lorenzo.studente@itispaleocapa.it>
2024-10-07 13:32:26 +0200

    .

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -3,38 +3,48 @@ public class Giocatore {
     private boolean capitano;
     private int gol;
 
-public Giocatore( String nomeGiocatore, boolean isCapitano, int goal){
-    nome= nomeGiocatore;
-    capitano=isCapitano;
-    gol=goal;
-}
-public Giocatore(String nomeGiocatore){
-    nome= nomeGiocatore;
-}
-    public Giocatore(int goal){
-        gol=goal;
+    public Giocatore(String nomeGiocatore, boolean isCapitano, int goal) {
+        setNome(nome);
+        setCapitano(capitano);
+        setGol(gol);
     }
-    public Giocatore(boolean isCapitano){
-        capitano=isCapitano;
+
+    public Giocatore(String nomeGiocatore) {
+        setNome(nome);
     }
-    public Giocatore(String nomeGiocatore,boolean isCapitano){
-        nome= nomeGiocatore;
-        capitano=isCapitano;
+
+    public Giocatore(int goal) {
+        setGol(gol);
     }
-    public Giocatore( String nomeGiocatore,int goal){
-        nome= nomeGiocatore;
-        gol=goal;
+
+    public Giocatore(boolean isCapitano) {
+        setCapitano(capitano);
     }
-    public Giocatore(  boolean isCapitano, int goal){
-        capitano=isCapitano;
-        gol=goal;
+
+    public Giocatore(String nomeGiocatore, boolean isCapitano) {
+        setNome(nome);
+        setCapitano(capitano);
     }
-    public Giocatore(){
+
+    public Giocatore(String nomeGiocatore, int goal) {
+        setNome(nome);
+        setGol(gol);
+    }
+
+    public Giocatore(boolean isCapitano, int goal) {
+        setCapitano(capitano);
+        setGol(gol);
+    }
+
+    public Giocatore() {
+        setNome(nome);
+        setCapitano(capitano);
+        setGol(gol);
 
     }
 
-public String getNome(){
-    return nome;
+    public String getNome() {
+        return nome;
     }
 
     public int getGol() {
@@ -51,6 +61,12 @@ public String getNome(){
 
     public void setGol(int gol) {
         this.gol = gol;
+        if(gol<0){
+
+        }
+        else{
+            setGol(gol);
+        }
     }
 
     public void setNome(String nome) {
--- src/testGiocatore.java
+++ src/testGiocatore.java
@@ -1,6 +1,31 @@
 import java.util.Scanner;
-public class testGiocatore{
-    public static void main(String[]args){
 
+public class testGiocatore {
+    public static void main(String[] args) {
+        int scelta;
+        Scanner in = new Scanner(System.in);
+        Giocatore giocatore= new Giocatore("nome")
+        Giocatore[] squadra = new Giocatore[10];
+        do {
+            System.out.println("scegli tra una di queste funzionalità");
+            System.out.println("1: aggiungere un giocatore alla squadra, controllando che non ci sia più di un capitano");
+            System.out.println("2: visualizzare tutti i giocatori della squadra");
+            System.out.println("3: modificare i dati di un giocatore a scelta ");
+            System.out.println("4: cancellare un giocatore dalla squadra ");
+            System.out.println("5: visualizzare i giocatori che hanno realizzato più di 5 goal.");
+            System.out.println("6: visualizzare il nome del capitano");
+            System.out.println("7: assegnare il ruolo di capitano in modo casuale se non ancora presente");
+            System.out.println("0: fine");
+            scelta = in.nextInt();
+
+        } while (scelta != 0);
+        switch(scelta){
+            case 1 :
+                System.out.println("nome giocatore nuovo:");
+
+                for(int i=0;i< squadra.length;i++){
+
+                }
+        }
     }
 }
