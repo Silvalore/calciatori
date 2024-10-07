188e58e0234130f54c27ce43c035fdbe39143337
silva.21140 <silva.lorenzo.studente@itispaleocapa.it>
2024-10-07 12:37:14 +0200

    .

---
+++ src/Giocatore.java
@@ -0,0 +1,9 @@
+public class Giocatore {
+    private String nome;
+    private boolean capitano;
+    private int gol;
+
+public Giocatore( String nomeGiocatore, boolean isCapitano, int goal){
+
+}
+}
